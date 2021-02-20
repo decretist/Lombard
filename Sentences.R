ade3cbff46d1d899082b9771881eb73e0b4cdf78
Paul Evans <decretist@gmail.com>
2021-02-19 16:13:16 -0800

    Add features list

--- Sentences.R
+++ Sentences.R
@@ -2,11 +2,18 @@
 # February 2021
 setwd("~/Work/Lombard")
 library(stylo)
+files.to.analyze <- c("Gratian0.txt", "Gratian1.txt", "Gratian2.txt",
+                      "Lombard2.txt", "Lombard3.txt", "Lombard4.txt")
+writeLines(files.to.analyze, "files_to_analyze.txt")
 stylo.results = stylo(
   gui = TRUE,
+  features = "wordlist_quoteful.txt",
+  corpus.dir = "./corpora/quoteful",
   corpus.lang = "Latin.corr",
   mfw.min = 65, mfw.max = 65,
   delete.pronouns = TRUE,
+  use.existing.wordlist = FALSE,
+  use.custom.list.of.files = TRUE,
   analysis.type = "PCR",
   sampling = "normal.sampling",
   sample.size = 3600,
