93869db4d377a1f7102e111b6096ebab3531774c
Paul Evans <decretist@gmail.com>
2021-02-20 14:12:20 -0800

    Complete second experiment
    The first experiment included Gratian 0, 1, and 2.
    The second experiment excluded Gratian 0.

--- Sentences.R
+++ Sentences.R
@@ -1,7 +1,10 @@
+#
 # Paul Evans (paul.evans@aya.yale.edu)
 # February 2021
+#
 setwd("~/Work/Lombard")
 library(stylo)
+#
 files.to.analyze <- c("Gratian0.txt", "Gratian1.txt", "Gratian2.txt",
                       "Lombard2.txt", "Lombard3.txt", "Lombard4.txt")
 writeLines(files.to.analyze, "files_to_analyze.txt")
@@ -12,7 +15,26 @@ stylo.results = stylo(
   corpus.lang = "Latin.corr",
   mfw.min = 65, mfw.max = 65,
   delete.pronouns = TRUE,
-  use.existing.wordlist = TRUE,
+  use.existing.wordlist = FALSE,
+  use.custom.list.of.files = TRUE,
+  analysis.type = "PCR",
+  sampling = "normal.sampling",
+  sample.size = 3600,
+  pca.visual.flavour = "symbols",
+)
+print(stylo.results$features.actually.used)
+#
+files.to.analyze <- c("Gratian1.txt", "Gratian2.txt",
+                      "Lombard2.txt", "Lombard3.txt", "Lombard4.txt")
+writeLines(files.to.analyze, "files_to_analyze.txt")
+stylo.results = stylo(
+  gui = TRUE,
+  features = "wordlist_quoteful_2.txt",
+  corpus.dir = "./corpora/quoteful",
+  corpus.lang = "Latin.corr",
+  mfw.min = 65, mfw.max = 65,
+  delete.pronouns = TRUE,
+  use.existing.wordlist = FALSE,
   use.custom.list.of.files = TRUE,
   analysis.type = "PCR",
   sampling = "normal.sampling",
