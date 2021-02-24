28deaa9745386ff76ea77354ac2f3d2a850195eb
Paul Evans <decretist@gmail.com>
2021-02-24 13:32:57 -0800

    Complete second experiment with quoteless samples

--- Sentences.R
+++ Sentences.R
@@ -77,3 +77,21 @@ stylo.results = stylo(
   pca.visual.flavour = "symbols",
 )
 print(stylo.results$features.actually.used)
+#
+files.to.analyze <- c("Gratian1.txt", "Gratian2.txt",
+                      "Lombard2.txt", "Lombard3.txt", "Lombard4.txt")
+writeLines(files.to.analyze, "files_to_analyze.txt")
+stylo.results = stylo(
+  gui = FALSE,
+  features = "wordlist_quoteless_2.txt",
+  corpus.dir = "./corpora/quoteless",
+  corpus.lang = "Latin.corr",
+  mfw.min = 65, mfw.max = 65,
+  delete.pronouns = TRUE,
+  use.custom.list.of.files = TRUE,
+  analysis.type = "PCR",
+  sampling = "normal.sampling",
+  sample.size = 3600,
+  pca.visual.flavour = "symbols",
+)
+print(stylo.results$features.actually.used)
